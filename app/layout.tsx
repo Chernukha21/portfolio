5afd2ea69a36cce7ffbc7f33119793e75a9aa915
Chernukha21 <Chorniy2008@ukr.net>
2025-05-05 20:56:09 +0300

    added changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { ThemeProvider } from '@/app/provider';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.css';
+import React from "react";
+import { ThemeProvider } from "@/app/provider";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'DevPortfolio',
-  description: 'My web page',
+  title: "DevPortfolio",
+  description: "My web page",
 };
 
 export default function RootLayout({
