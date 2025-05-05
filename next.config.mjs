df1d7ff85516219f62dbedd92b88ff0fd51f37c7
Chernukha21 <Chorniy2008@ukr.net>
2025-05-05 23:16:45 +0300

    changed system settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,35 +1,34 @@
-import {withSentryConfig} from '@sentry/nextjs';
-/** @type {import('next').NextConfig} */
+import { withSentryConfig } from "@sentry/nextjs";
+
+/** @type {import("next").NextConfig} */
 const nextConfig = {
   // output: "export",
   typescript: {
     ignoreBuildErrors: true
   },
   eslint: {
-    ignoreDuringBuilds: true,
-  },
+    ignoreDuringBuilds: true
+  }
 };
+
 
 export default withSentryConfig(nextConfig, {
 // For all available options, see:
 // https://github.com/getsentry/sentry-webpack-plugin#options
 
-org: "nocompany-jm",
-project: "javascript-nextjs",
-
-// Only print logs for uploading source maps in CI
-silent: !process.env.CI,
-
+// Suppresses source map uploading logs during build
+  silent: true,
+  org: "nocompany-jm",
+  project: "javascript-nextjs",
+}, {
 // For all available options, see:
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
 // Upload a larger set of source maps for prettier stack traces (increases build time)
-widenClientFileUpload: true,
+  widenClientFileUpload: true,
 
-// Automatically annotate React components to show their full name in breadcrumbs and session replay
-reactComponentAnnotation: {
-enabled: true,
-},
+// Transpiles SDK to be compatible with IE11 (increases bundle size)
+  transpileClientSDK: true,
 
 // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
 // This can increase your server load as well as your hosting bill.
@@ -38,14 +37,14 @@ enabled: true,
 // tunnelRoute: "/monitoring",
 
 // Hides source maps from generated client bundles
-hideSourceMaps: true,
+  hideSourceMaps: true,
 
 // Automatically tree-shake Sentry logger statements to reduce bundle size
-disableLogger: true,
+  disableLogger: true,
 
-// Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+// Enables automatic instrumentation of Vercel Cron Monitors.
 // See the following for more information:
 // https://docs.sentry.io/product/crons/
 // https://vercel.com/docs/cron-jobs
-automaticVercelMonitors: true,
+  automaticVercelMonitors: true,
 });
